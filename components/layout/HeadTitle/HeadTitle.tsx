a42438e2f37e0d7335d0b688b56eaadeeb658f68
theforbiddenpool <contact@theforbiddenpool.com>
2021-09-10 19:01:04 +0200

    Add HeadTitle component

---
+++ components/layout/HeadTitle/HeadTitle.tsx
@@ -0,0 +1,17 @@
+import Head from 'next/head'
+
+interface Props {
+  title: string;
+}
+
+function HeadTitle({ title }: Props) {
+  return (
+    <Head>
+      <title>
+        {title}
+      </title>
+    </Head>
+  )
+}
+
+export default HeadTitle
---
+++ components/layout/index.ts
@@ -0,0 +1,4 @@
+/* eslint-disable import/prefer-default-export */
+import HeadTitle from './HeadTitle/HeadTitle';
+
+export { HeadTitle }

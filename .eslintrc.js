5f8cacd770d64492fea7743c5dc7a8446848a257
theforbiddenpool <contact@theforbiddenpool.com>
2021-09-10 18:17:06 +0200

    Setup Jest + React Testing Library

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,15 @@
 module.exports = {
+  plugins: ['jest'],
   parserOptions: {
     project: './tsconfig.json',
   },
-  extends: ['next', 'next/core-web-vitals', 'airbnb', 'airbnb-typescript'],
+  extends: [
+    'next',
+    'next/core-web-vitals',
+    'airbnb',
+    'airbnb-typescript',
+    'plugin:jest/recommended',
+  ],
   rules: {
     'react/react-in-jsx-scope': 'off',
     'react/jsx-props-no-spreading': [1, {
---
+++ jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  clearMocks: true,
+  coverageDirectory: '.coverage',
+  moduleNameMapper: {
+    '\\.(css|less|scss|sss|styl)$': '<rootDir>/node_modules/jest-css-modules',
+  },
+  testPathIgnorePatterns: ['/node_modules/', '/.next/'],
+  setupFilesAfterEnv: ['./jest.setup.js'],
+  testEnvironment: 'jsdom',
+}
